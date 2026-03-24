66e90c634d7bd84944d5c159b9c6a9240b6dc94b
Abdihafiid Ahmed <pizma0901@gmail.com>
2026-03-24 10:55:20 +0100

    header for buffer could be changed later on

--- Headers/morse_buffer.h
+++ Headers/morse_buffer.h
@@ -1,1 +1,13 @@
 #define MORSE_BUFFER_H
+
+void circlebuff_init(struct morse_buffer, *buff, int size);
+
+int circlebuff_resize(struct morse_buffer, *buff, int new_size);
+
+
+int circlebuff_full(struct morse_buffer, *buff);
+int circlebuff_empty(struct morse_buffer, *buff);
+int circlebuff_freespace(struct morse_buffer, *buff);
+
+int circlebuff_add(struct morse_buffer, *buff, char c);
+int circlebuff_remove(struct morse_buffer, *buff, char *c);
