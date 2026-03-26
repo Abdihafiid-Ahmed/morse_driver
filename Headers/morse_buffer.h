4a739728790c820d48a07bd34711077c0eaccd60
absad24 <absad24@student.sdu.dk>
2026-03-26 01:44:17 +0100

    parts of the buffer and header file with comments

--- Headers/morse_buffer.h
+++ Headers/morse_buffer.h
@@ -1,13 +1,23 @@
 #define MORSE_BUFFER_H
 
-void circlebuff_init(struct morse_buffer, *buff, int size);
+#define MORSE_BUFFER_MAX 1024 //maximum capacity  
 
-int circlebuff_resize(struct morse_buffer, *buff, int new_size);
+struct morse_buffer{
+  char  data[MORSE_BUFFER_MAX]; ///static data
+  int   head; ///writing data
+  int   tail; ////reading 
+  int   count; ////currnet number of elements stored
+  int   size; //// active capacity limit
+};
+
+void circlebuff_init(struct morse_buffer *buff, int size);
+
+int circlebuff_resize(struct morse_buffer *buff, int new_size);
 
 
-int circlebuff_full(struct morse_buffer, *buff);
-int circlebuff_empty(struct morse_buffer, *buff);
-int circlebuff_freespace(struct morse_buffer, *buff);
+int circlebuff_full(struct morse_buffer *buff);
+int circlebuff_empty(struct morse_buffer *buff);
+int circlebuff_freespace(struct morse_buffer *buff);
 
-int circlebuff_add(struct morse_buffer, *buff, char c);
-int circlebuff_remove(struct morse_buffer, *buff, char *c);
+int circlebuff_add(struct morse_buffer *buff, char c);
+int circlebuff_remove(struct morse_buffer *buff, char *c);
--- implementation/morse_buffer.c
+++ implementation/morse_buffer.c
@@ -1,1 +1,51 @@
-#include <stdio.h>
+#include "morse_buffer.h"
+#include <linux/string.h>
+#include <linux/errno.h>
+
+
+void circlebuff_init(struct morse_buffer *buff, int size){
+  memset(buff->data, 0, MORSE_BUFFER_MAX);
+  buff->head  = 0;
+  buff->tail  = 0;
+  buff->count = 0;
+  buff->size  = size;
+
+  if (buff->size > MORSE_BUFFER_MAX){
+    buff->size = MORSE_BUFFER_MAX;
+  else
+    buff->size = size;
+  }
+  }
+
+//resize if meet conditions
+int circlebuff_resize(struct morse_buffer *buff, int new_size){
+    if (new_size < 1 || new_size > MORSE_BUFFER_MAX)
+      return -EINVAL;
+
+    if (new_size < buff->count)
+        return -EINVAL;
+    buff->size = new_size;
+    return 0;
+  }
+
+///////check if buffer has reached capacity
+int circlebuff_full(struct morse_buffer *buff)
+{
+  return buff->count == buff->size;
+}
+
+///check if buffer is empty
+int circlebuff_empty(struct morse_buffer *buff)
+{
+  return buff->count == 0;
+}
+
+//calculate reamaining slots  
+int circlebuff_freespace(struct morse_buffer *buff)
+{
+  return buff->size - buf->count;
+
+}
+
+
+} 
